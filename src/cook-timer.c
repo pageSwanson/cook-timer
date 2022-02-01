bca8a0f14b2cc86aee9f05bfcd7b962f5ab1446c
K Swa <kpswanson@gatech.edu>
2022-01-31 23:46:11 -0500

    comments on structure

--- src/cook-timer.c
+++ src/cook-timer.c
@@ -2,5 +2,23 @@
 #include <stdio.h>
 
 int main (int argc, char* argv[]) {
+  // parse args (flags)
+  //  translation from user flags to functions
+  // return if invalid
+  // produce init structure (array of timers with labels, durations)
+  //  use function instructions from translation to populate list
+  // init ncurses
+  // start timers
+  // print curses from list of timers
+  // while (1)
+  //  manage + update timers (update countdown value)
+  //  parse commands (pause, quit, pause <timer-label>, add <timer-label> xx:xx:xx)
+  //  translation to functions
+  //  produce updated list
+  //  print curses from list
+  // outside of loop
+  //  getch()
+  //  terminate outstanding curses
+
   printf("cooking timers coming soon ...\n");
 }
